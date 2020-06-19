a896046ff162b7ea107ddf7e19bd5804ec156ee8
Marian Bucatariu <bucatariumariantiberiu@gmail.com>
2020-06-19 18:47:00 +0300

    Changed popups to use ImagePopout.

--- src/module/renderChatMessageHook.ts
+++ src/module/renderChatMessageHook.ts
@@ -8,35 +8,20 @@ class RenderChatMessageHook {
         }
     }
 
-    private _imagePopoutTemplate(image: any, previewButton: any): any {
-        const that = this;
-        const container = document.createElement('DIV');
-        const img = document.createElement('img');
-
-        container.className = 'chat-images-dialog-preview';
-        img.className = 'chat-images-dialog-image';
-
-        img.src = image;
-        img.alt = 'image';
-
-        container.appendChild(img);
-        container.addEventListener('click', event => {
-            that._togglePreviewButton(previewButton, false);
-            container?.parentNode.removeChild(container);
-        });
-
-        return container;
-    }
-
     private _buttonClickEventListener(button: any, container: any): void {
         const image = container.querySelector('.chat-images-image');
         const previewButton = container.querySelector('.chat-images-expand-preview-button');
         if (!image || !previewButton) {
             return;
         }
-        this._togglePreviewButton(previewButton, true);
-        const popout = this._imagePopoutTemplate(image.src, previewButton);
-        document.body.prepend(popout);
+
+        const popout = new ImagePopout(image.src, {
+            title: "Chat Image",
+            editable: false,
+            shareable: false,
+        });
+
+        popout.render(true);
     }
 
     public addImagePreviewButton(_0: any, html: any, _1: any): void {
