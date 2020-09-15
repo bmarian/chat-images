18a62ea4cbd0aaa2b8f2352a1594a435e7339e48
Marian <33843056+bmarian@users.noreply.github.com>
2020-09-15 18:26:48 +0300

    [WIP] Refactor: added TODOs

--- src/chat-images.ts
+++ src/chat-images.ts
@@ -5,8 +5,10 @@ import renderChatMessage from "./module/hooks/RenderChatMessage";
 import Init from "./module/hooks/Init";
 
 Hooks.once('init', Init.initHook.bind(Init));
+Hooks.on('renderSidebarTab', RenderSidebarTab.renderSidebarTabHook.bind(RenderSidebarTab));
 
 /**
+ * TODO
  * Adding a hook on preCreateChatMessage to get the message before is posted in chat and to
  * manipulate the data
  */
@@ -22,11 +24,10 @@ Hooks.on('preCreateChatMessage', (message: any, options: any): void => {
 });
 
 /**
+ * TODO
  * Adding a hook on renderChatMessage to add events on all the preview buttons when
  * they get rendered
  */
 Hooks.on('renderChatMessage', (_0: any, html: any): void => {
     renderChatMessage.addImagePreviewButton(html);
 });
-
-Hooks.on('renderSidebarTab', RenderSidebarTab.renderSidebarTabHook.bind(RenderSidebarTab));
