3a53973b2a6ef655e770a7b47e08ecd02db10f11
Marian <33843056+bmarian@users.noreply.github.com>
2020-10-29 21:25:19 +0200

    Refactored the Init class

--- src/chat-images.ts
+++ src/chat-images.ts
@@ -1,9 +1,12 @@
 import PreCreateChatMessage from "./module/hooks/PreCreateChatMessage";
 import RenderSidebarTab from "./module/hooks/RenderSidebarTab"
 import RenderChatMessage from "./module/hooks/RenderChatMessage";
-import Init from "./module/hooks/Init";
+import {createUploadFolderIfMissing, registerSettings} from "./module/settings";
 
-Hooks.once('init', Init.initHook.bind(Init));
+Hooks.once('init', async () => {
+    registerSettings();
+    await createUploadFolderIfMissing();
+});
 Hooks.on('renderSidebarTab', RenderSidebarTab.renderSidebarTabHook.bind(RenderSidebarTab));
 Hooks.on('preCreateChatMessage', PreCreateChatMessage.preCreateChatMessageHook.bind(PreCreateChatMessage));
 Hooks.on('renderChatMessage', RenderChatMessage.RenderChatMessageHook.bind(RenderChatMessage));
