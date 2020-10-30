d2dd99585d84f2680c67cfbbba487e40bafaf754
Marian <33843056+bmarian@users.noreply.github.com>
2020-10-30 16:11:16 +0200

    WIP: Rewrite file conversion logic

--- src/module/imageManager.ts
+++ src/module/imageManager.ts
@@ -38,4 +38,23 @@ const renderPopout = (url: string): Application =>
 // the on click event that is added for all the images in the chat
 const createPopoutOnClick = (imgHTML: HTMLImageElement): Application => renderPopout(imgHTML.src);
 
+
+//============================\\
+//   CONVERT FILES TO IMAGES  \\
+//============================\\
+
+// returns the file extension from a given file name
+const getFileExtensionFromName = (fileName: string): string => fileName.substring(fileName.lastIndexOf('.'), fileName.length) || null;
+
+// generates a long random string used as a file name
+const generateRandomString = (): string => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+// generates a random file name keeping the file extension
+const generateRandomFileName = (fileName: string): string => {
+    const fileExtension = getFileExtensionFromName(fileName);
+    if (!fileExtension) return fileName;
+
+    return fileExtension ? generateRandomString() + fileExtension : fileName;
+}
+
 export {convertMessageToImage, createPopoutOnClick};
