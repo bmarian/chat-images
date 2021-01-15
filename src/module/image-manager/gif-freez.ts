a138d0636f10afaa2774174c597b35cdcfc1e07d
Marian <33843056+bmarian@users.noreply.github.com>
2021-01-15 20:57:54 +0200

    * added support for freezing gifs

---
+++ src/module/image-manager/gif-freez.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Freezes the gifs in the chat
+ *
+ * @param {HTMLElement} img
+ */
+function freezeGif(img) {
+  const src = img.src;
+  const options = {
+    warnings: false,
+    overlay: true,
+  };
+
+  const isEmbeddedGif = src.startsWith('data:image/gif');
+  // @ts-ignore
+  if (isEmbeddedGif) return new Freezeframe(img, options);
+
+  const isGifUrl = src.endsWith('gif');
+  // @ts-ignore
+  if (isGifUrl) return new Freezeframe(img, options);
+}
+
+export {
+  freezeGif,
+};
