368bc191ade153e7d05348f5f7b2e5a0225cb8c5
bmarian <33843056+bmarian@users.noreply.github.com>
2022-09-11 21:53:54 +0300

    added rxjs

--- src/chat-images.ts
+++ src/chat-images.ts
@@ -1,3 +1,15 @@
 import "./styles/chat-images.scss"
+import {fromEvent} from "rxjs"
 
-console.log('chat images')
+Hooks.once("init", () => {
+  console.log("Init")
+})
+
+Hooks.on('renderSidebarTab', (_0: any, sidebarJQueryElement: JQuery) => {
+  const sidebarHTMLElement: HTMLElement = sidebarJQueryElement[0]
+  const chat: HTMLElement | null = sidebarHTMLElement.querySelector('#chat-message')
+  if (!chat) return;
+
+  fromEvent(chat, 'paste').subscribe((evt) => console.log(evt))
+  fromEvent(chat, 'drop').subscribe((evt) => console.log(evt))
+})
--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,25 @@ import {normalizePath, defineConfig} from "vite"
 import {viteStaticCopy} from "vite-plugin-static-copy"
 
 const everyWordToUpperCase = (sentence: string) => sentence
-  .split(' ')
+  .split(" ")
   .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-  .join(' ')
+  .join(" ")
 
 export default defineConfig({
   build: {
     watch: {},
-    sourcemap: 'inline',
+    sourcemap: "inline",
     lib: {
-      entry: normalizePath(resolve(__dirname, 'src/chat-images.ts')),
+      entry: normalizePath(resolve(__dirname, "src/chat-images.ts")),
       name: everyWordToUpperCase(moduleName),
       fileName: moduleName,
-      formats: ['es'],
+      formats: ["es"],
     },
     rollupOptions: {
       // @ts-ignore
       output: {
         assetFileNames(assetInfo) {
-          return assetInfo.name === 'style.css' ? `${moduleName}.css` : assetInfo.name
+          return assetInfo.name === "style.css" ? `${moduleName}.css` : assetInfo.name
         }
       }
     },
@@ -31,16 +31,16 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: 'src/templates/*',
-          dest: 'templates',
+          src: "src/templates/*",
+          dest: "templates",
         },
         {
-          src: 'src/languages/*',
-          dest: 'languages',
+          src: "src/languages/*",
+          dest: "languages",
         },
         {
-          src: 'module.json',
-          dest: '',
+          src: "module.json",
+          dest: "",
         }
       ]
     })
