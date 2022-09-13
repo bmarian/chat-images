a393448e6154eb93a7434ab9491dd207c4915b7a
bmarian <33843056+bmarian@users.noreply.github.com>
2022-09-13 10:23:16 +0300

    Added more images to imageReg

--- src/scripts/processors/MessageProcessor.ts
+++ src/scripts/processors/MessageProcessor.ts
@@ -1,7 +1,7 @@
 import {t} from '../utils/Utils'
 
 const imageMarkdownReg = /!\s*ci\s*\|\s*(.+?)\s*!/gi
-const imageReg = /\w+\.(jpg|jpeg|gif|png|tiff|bmp)/ig
+const imageReg = /\w+\.(gif|png|jpg|jpeg|webp|svg|psd|bmp|tif)/gi
 
 const imageTemplate = (src: string): string => `<div class="ci-message-image"><img src="${src}" alt="${t('unableToLoadImage')}"></div>`
 
